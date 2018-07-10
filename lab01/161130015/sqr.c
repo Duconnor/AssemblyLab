cede8f691d8bc3a7e11156adae6fb7be077e87cc
root <duconnor@outlook.com>
2018-07-10 01:22:41 +0000

    add lab1

---
+++ lab01/161130015/sqr.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    float i, j;
+    i = 40000;
+    j = i * i;
+    printf("The 40000 * 40000 is %f\n", j);
+    i = 50000;
+    j = i * i;
+    printf("The 50000 * 50000 is %f\n", j);
+    return 0;
+}
